484c00b1745b8b6edaf63600ce094c8d29dd9c2d
Peter Koscelansky <koscelansky@live.com>
2021-04-22 13:32:39 +0200

    GUI

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 # tazko hacknutelne (bezpecne)
-# lahko kopirovali hesla
-# generovat hesla <--
 
 import secrets
 import string
 import json
+import tkinter
+import webbrowser
 from os import path
 
-DATA_FILENAME = 'data.txt'
+DATA_FILENAME = 'data.json'
 
 PASSWORD = [
 ]
-
 
 if path.exists(DATA_FILENAME):
     try:
@@ -30,6 +29,9 @@ def generate_password(length, **kwargs):
 
     Example:
     """
+
+
+
     unknown = ''
     for (k, v) in kwargs.items():
         if k == 'lowercase':
@@ -47,6 +49,9 @@ def generate_password(length, **kwargs):
         else:
             raise KeyError(k + ' is not valid parameter.')
 
+    if unknown == '':
+        unknown = string.ascii_lowercase + string.digits
+
     password = ''    
     for i in range(length):
         password += secrets.choice(unknown)
@@ -54,43 +59,148 @@ def generate_password(length, **kwargs):
     return password
 
 
-def add():
-    web = input('Zadaj web adresu ')
-    username = input('Zadaj užívateľské meno ')
-    password = input('Zadaj heslo (ak prázdne vygeneruje sa nové) ')
+root = tkinter.Tk()
+root.title('Best password manager')
+root.minsize(200, 150)
 
-    if password == '':
-        password = generate_password(16, lowercase=True, uppercase=True, digit=True)
+# Creating Listbox
+records = tkinter.Listbox(root)
 
-    PASSWORD.append({
-        'web': web,
-        'username': username,
-        'password': password
-    })
+def copyToCliboard(entry):
+    def result():
+        root.clipboard_clear()
+        root.clipboard_append(entry.get())
 
-def print_menu():
-    print('ADD - Zadávanie nového vstupu')
-    print('DEL - Mazanie položky')
-    print('VIEW - Zobrazenie položky')
-    print('EXIT - Ukončenie programu')
+    return result
 
-print('Best password managed')
+def goToUrl(entry):
+    def result():
+        webbrowser.open(entry.get())
 
-while True:
-    print_menu()
-    cmd = input('Zadaj príkaz ')
-    if cmd == 'ADD':
-        add()
-    elif cmd == 'DEL':
-        pass
-    elif cmd == 'VIEW':
-        pass
-    elif cmd == 'EXIT':
-        break
-    else:
-        print('Nesprávny príkaz.')
+    return result
 
-with open('data.txt', 'w') as outfile:
+def add(event):
+    top = tkinter.Toplevel(root)
+    top.title('Add')
+    top.transient(root)
+    top.grab_set()
+
+    l1 = tkinter.Label(top, text = 'Web:')
+    l1.grid(column=0, row=0, pady=5, padx=5, sticky=tkinter.E)
+
+    webText = tkinter.Entry(top)
+    webText.grid(column=1, row=0, pady=5, padx=5)
+
+    l2 = tkinter.Label(top, text = 'Username:')
+    l2.grid(column=0, row=1, pady=5, padx=5, sticky=tkinter.E)
+
+    usernameText = tkinter.Entry(top)
+    usernameText.grid(column=1, row=1, pady=5, padx=5)
+
+    l3 = tkinter.Label(top, text = 'Password:')
+    l3.grid(column=0, row=2, pady=5, padx=5, sticky=tkinter.E)
+
+    passwordText = tkinter.Entry(top)
+    passwordText.grid(column=1, row=2, pady=5, padx=5)
+
+    def ok():
+        password = passwordText.get()
+        if password == '':
+            password = generate_password(16)
+
+        PASSWORD.append({
+            'web': webText.get(),
+            'username': usernameText.get(),
+            'password': password
+        })
+
+        records.insert(tkinter.END, PASSWORD[len(PASSWORD) - 1]['web'])
+
+        top.destroy()
+
+    def cancel():
+        top.destroy()
+
+    okButton = tkinter.Button(top, text='OK', command=ok)
+    okButton.grid(column=0, row=3, pady=5)
+
+    cancelButton = tkinter.Button(top, text='Cancel', command=cancel)
+    cancelButton.grid(column=1, row=3, pady=5)
+   
+    x = root.winfo_x() + root.winfo_width() // 2 - top.winfo_reqwidth() // 2
+    y = root.winfo_y() + root.winfo_height() // 2 - top.winfo_reqheight() // 2
+    top.geometry(f'+{x}+{y}')
+
+    webText.focus_set()
+
+
+def view(event):
+    try:
+        i = event.widget.curselection()[0]
+    except:
+        return
+
+    webText.delete(0, tkinter.END)
+    webText.insert(0, PASSWORD[i]['web'])
+
+    usernameText.delete(0, tkinter.END)
+    usernameText.insert(0, PASSWORD[i]['username'])
+
+    passwordText.delete(0, tkinter.END)
+    passwordText.insert(0, PASSWORD[i]['password'])
+
+def delete(event):
+    try:
+        i = records.curselection()[0]
+    except:
+        return
+
+    del PASSWORD[i]
+    records.delete(i)
+
+# Inserting passwords
+for i in PASSWORD:
+    records.insert(tkinter.END, i['web'])
+   
+records.bind('<<ListboxSelect>>', view)
+records.grid(column=0, row=0, rowspan=3, columnspan=3)
+
+l1 = tkinter.Label(text = 'Web:')
+l1.grid(column=4, row=0, padx=5, sticky=tkinter.E)
+
+webText = tkinter.Entry()
+webText.grid(column=5, row=0)
+
+webButton = tkinter.Button(text='Open', command=goToUrl(webText))
+webButton.grid(column=6, row=0, padx=5)
+
+l2 = tkinter.Label(text = 'Username:')
+l2.grid(column=4, row=1, padx=5, sticky=tkinter.E)
+
+usernameText = tkinter.Entry()
+usernameText.grid(column=5, row=1)
+
+usernameButton = tkinter.Button(text='Copy', command=copyToCliboard(usernameText))
+usernameButton.grid(column=6, row=1, padx=5)
+
+l3 = tkinter.Label(text = 'Password:')
+l3.grid(column=4, row=2, padx=5, sticky=tkinter.E)
+
+passwordText = tkinter.Entry()
+passwordText.grid(column=5, row=2)
+
+passwordButton = tkinter.Button(text='Copy', command=copyToCliboard(passwordText))
+passwordButton.grid(column=6, row=2, padx=5)
+
+addButton = tkinter.Button(text = 'Add')
+addButton.bind('<Button>', add)
+addButton.grid(column=0, row=4)
+
+removeButton = tkinter.Button(text = 'Remove')
+removeButton.bind('<Button>', delete)
+removeButton.grid(column=1, row=4)
+
+root.mainloop()
+
+with open(DATA_FILENAME, 'w') as outfile:
     json.dump(PASSWORD, outfile)
-
-print(PASSWORD)
